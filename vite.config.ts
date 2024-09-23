086743d1cf9dbfdc1929bf4973e4668e85f3a2ab
vvalvvizal <vvalvvizal@gmail.com>
2024-09-23 15:32:57 +0900

    deploy : 빌드 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
+export default {
+  base: "/stopwatch/",
   server: {
-    open: true, // 서버 시작 시 브라우저 열림
-    port: 3000, // 포트 번호 변경
+    open: true,
+    port: 3000,
   },
-});
+};
